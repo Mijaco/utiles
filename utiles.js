fce9490ab1f42a75af53fcbc1cf16c644c47dc38
Mijaco <casio.soe@gmail.com>
2017-05-03 11:27:16 -0500

    utiles.js
    utiles.js

---
+++ utiles.js
@@ -0,0 +1,9 @@
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+loadCss ('js/libs/jquery.mobile-1.4.0-beta.1.min.css');
